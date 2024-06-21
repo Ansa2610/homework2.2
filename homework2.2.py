cb78cc5ccde2c6df63cda9b5d73132192e52f522
Ansa2610 <asavilova@gmail.com>
2024-06-21 10:35:09 +0300

    Add files via upload

---
+++ homework2.2.py
@@ -0,0 +1,5 @@
+Homework_number = 12
+Hours = 1.5
+Course_name = 'Python,'
+hours_per_homework = (Hours / Homework_number)
+print('Курс:', Course_name, 'всего задач:', Homework_number,  'затрачено часов:', Hours,',','среднее время выполнения:', hours_per_homework, 'часа.')
